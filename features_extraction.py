features are fixed now, they return mean and variance, also the spectral_bandwidth feature is added

features_extraction.py
@@ -3,6 +3,10 @@ import processing
 import visualizing
 import matplotlib.pyplot as plt
 import sklearn
+import numpy as np
+
+
+
 # zero-crossing Rate
 # to visualize the zero crossing
 def zero_crossing(sound_file):
@@ -17,11 +21,11 @@ def zero_crossing(sound_file):
 # calculated as the weighted mean of the frequencies present in the sound
 # So spectral centroid for blues song will lie somewhere near the middle of
 # its spectrum while that for a metal song would be towards its end.
-#TODO the function doesnt' retrurn any particular feature, just draws the graph
+
 def spectral_centroid(sound_file):
     x, sr = processing.imp_sound(sound_file)
     spectral_centroids = librosa.feature.spectral_centroid(x, sr=sr)[0]
-    print(spectral_centroids.shape)
+    #print(spectral_centroids.shape)
     # computing the time variable for visualization
     frames = range(len(spectral_centroids))
     t = librosa.frames_to_time(frames)
@@ -29,11 +33,12 @@ def spectral_centroid(sound_file):
     librosa.display.waveshow(x, sr=sr, alpha=0.4)
     plt.plot(t, processing.normalize(spectral_centroids), color='r')
     plt.show()
+    return(spectral_centroids.var(), spectral_centroids.mean())
 
 
 # it is the measure of the shape of the signal, it shows the frequency below which
 # a specified percentage of the total spectral energy lies
-#TODO define what will be returned as a feature
+
 def spectral_rollof(sound_file):
     x, sr = processing.imp_sound(sound_file)
     spectral_rollof = librosa.feature.spectral_rolloff(x+0.01, sr=sr)[0]
@@ -42,22 +47,24 @@ def spectral_rollof(sound_file):
     t = librosa.frames_to_time(frames)
     plt.plot(t, processing.normalize(spectral_rollof), color='r')
     plt.show()
+    return (spectral_rollof.var(), spectral_rollof.mean())
 
 #small set of features that describe the overall shape of a spectral envelope
-#TODO learn what is standardization doing here and find the return value
+
 def mel_coef(sound_file):
     x, sr = processing.imp_sound(sound_file)
     visualizing.show_wave((x, sr))
     mfccs = librosa.feature.mfcc(x, sr=sr)
     mfccs = sklearn.preprocessing.scale(mfccs, axis=1) #standardization of the variables
-    print(mfccs.mean(axis=1))
-    print(mfccs.var(axis=1))
+    #print(mfccs.mean(axis=1))
+    #print(mfccs.var(axis=1))
     librosa.display.specshow(mfccs, sr=sr, x_axis='time')
     plt.show()
+    return(mfccs.var(axis=1), mfccs.mean(axis=1))
 
 
 #chroma frequencies - entire spectrum is projected onto 12 bins representing 12 distinct semitones
-#TODO choose what it returns
+
 def chroma_freq(sound_file):
     x, sr = processing.imp_sound(sound_file)
     hop_len = 512
@@ -65,6 +72,14 @@ def chroma_freq(sound_file):
     plt.figure(figsize=(15,5))
     librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length=hop_len, cmap='coolwarm')
     plt.show()
+    return (chromagram.var(), chromagram.mean())
+
+# spectral bandwidth of the track
+def spectral_bandwidth(sound_file):
+    x, sr = processing.imp_sound(sound_file)
+    band = librosa.feature.spectral_bandwidth(x, sr)
+    print(band.shape)
+    return (band.var(axis=1), band.mean(axis=1))
 
 
 
@@ -73,8 +88,9 @@ def chroma_freq(sound_file):
 
 
 
-chroma_freq(processing.PATH3)
-#spectral_rollof(processing.PATH)
-#spectral_centroid(processing.PATH)
-#mel_coef(processing.PATH3)
-#zero_crossing(processing.PATH)
+#print(chroma_freq(processing.PATH))
+#print(spectral_rollof(processing.PATH))
+#print(spectral_centroid(processing.PATH))
+#print(mel_coef(processing.PATH))
+#print(zero_crossing(processing.PATH))
+print(spectral_bandwidth(processing.PATH))