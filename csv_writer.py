19d8b916f4e2e153fcd08160adf670b3fffc072b
s2422980 <y.budnyk@student.utwente.nl>
2022-07-02 14:35:41 +0200

    the information retreival and csv file writing is adjusted according to every track being an object now

--- csv_writer.py
+++ csv_writer.py
@@ -10,19 +10,19 @@ HEADER = ['track_id', 'name', 'zero crossings', 'spectral centroid variance', 's
           'mel coefficient variance', 'mel coefficient mean', 'chroma frequency variance', 'chroma frequncy mean', 'spectral bandwidth variance',
           'spectral bandwidth mean', 'genre']
 
-track1 = (processing.PATH, 'alternative rock')
-track2 = (processing.PATH2, 'tech house')
-track3 = (processing.PATH3, 'alternative rock')
-track4 = (processing.PATH4, 'rap')
-tracks = (track1, track2, track3, track4)
-track5 = Track(path='/Users/zhenyabudnyk/PycharmProjects/musicProject/sox_transformer/genres_original/hiphop/hiphop.00002.wav')
+#track1 = (processing.PATH, 'alternative rock')
+#track2 = (processing.PATH2, 'tech house')
+#track3 = (processing.PATH3, 'alternative rock')
+#track4 = (processing.PATH4, 'rap')
+#tracks = (track1, track2, track3, track4)
+#track5 = Track(path='/Users/zhenyabudnyk/PycharmProjects/musicProject/sox_transformer/genres_original/hiphop/hiphop.00002.wav')
 
 # this method is doing parsing through the folders of the genres folder and through their music files
 def parser(dir_path):
     tracks = list()
-    for folder in os.listdir('/Users/zhenyabudnyk/PycharmProjects/musicProject/sox_transformer/genres_original'):
-        for song in os.listdir('/Users/zhenyabudnyk/PycharmProjects/musicProject/sox_transformer/genres_original'+'/'+ folder):
-            new_track = Track('/Users/zhenyabudnyk/PycharmProjects/musicProject/sox_transformer/genres_original'+'/'+ folder + '/' + song)
+    for folder in os.listdir(dir_path):
+        for song in os.listdir(dir_path+'/'+ folder):
+            new_track = Track(dir_path+'/'+ folder + '/' + song)
             new_track.setGenre(folder)
             tracks.append(new_track)
     return tracks
@@ -33,38 +33,38 @@ def parser(dir_path):
 
 # method which generates the list of data
 # the data list will be used to make csv files later
-def data_collector(track):
+def data_collector(track: Track):
     data = list()
-    data.append(processing.name_retriever(track[0]))
-    zc = zero_crossing(track[0])
+    data.append(track.name)
+    zc = zero_crossing(track.path)
     data.append(zc)
     print(f'here is {zc}')
-    scv, scm = spectral_centroid(track[0])
+    scv, scm = spectral_centroid(track.path)
     data.append(scv)
     data.append(scm)
     print(f'here is {scv}')
     print(f'here is {scm}')
-    srv, srm = spectral_rollof(track[0])
+    srv, srm = spectral_rollof(track.path)
     data.append(srv)
     data.append(srm)
     print(f'here is {srv}')
     print(f'here is {srm}')
-    mcv, mcm = mel_coef(track[0])
+    mcv, mcm = mel_coef(track.path)
     data.append(mcv)
     data.append(mcm)
     print(f'here is {mcv}')
     print(f'here is {mcm}')
-    cfv, cfm = chroma_freq(track[0])
+    cfv, cfm = chroma_freq(track.path)
     data.append(cfv)
     data.append(cfm)
     print(f'here is {cfv}')
     print(f'here is {cfm}')
-    sbv, sbm = spectral_bandwidth(track[0])
+    sbv, sbm = spectral_bandwidth(track.path)
     data.append(sbv)
     data.append(sbm)
     print(f'here is {sbv}')
     print(f'here is {sbm}')
-    data.append(track[1])
+    data.append(track.getGenre())
     return data
 
 # this method inputs list of tracks and returns makes the csv file with all the features
@@ -82,5 +82,13 @@ def writer(tracks):
 
 #writer(tracks)
 
-for i in parser('/Users/zhenyabudnyk/PycharmProjects/musicProject/sox_transformer/genres_original'):
-    print(i.name)
+def run():
+    tracks = parser('/Users/zhenyabudnyk/PycharmProjects/musicProject/sox_transformer/genres_original')
+    writer(tracks)
+
+run()
+
+
+
+#for i in parser('/Users/zhenyabudnyk/PycharmProjects/musicProject/sox_transformer/genres_original'):
+    #print(i.name)
