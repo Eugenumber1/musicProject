0e92ee59f23d7f5c3d00c3d38e420e6711e018d1
s2422980 <y.budnyk@student.utwente.nl>
2022-07-04 18:17:55 +0200

    data preprocessing is done

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,9 +6,10 @@ import sklearn.model_selection
 # this method splits the variables in the data into dependent and independent ones
 # X - independent variables, or features
 # y - what we want to predict, music genre in our case
+# we take name and track id out of the context as those have no relation to the independent variables
 def split_variables(pathname):
     dataset = pd.read_csv(pathname)
-    X = dataset.iloc[:, :-1].values
+    X = dataset.iloc[:,2 :-1].values
     y = dataset.iloc[:, -1].values
     return X, y
 
@@ -18,8 +19,19 @@ def split_train_test(X, y):
     return X_train, X_test, y_train, y_test
 
 
+# this method will do the data scalling (choosing between standardization and normalisation)
+# for classification we need to scale only the numerical data, which is - independent variables
+# the dependent variable is a class(string) so we can't scale it
+# decision - to use standard scaler at first and evaluate the accuracy of the algo later
+def scaler(X_train, X_test):
+    scaler = sklearn.preprocessing.StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+    return X_train, X_test
+
 
 #print(split_variables('/Users/zhenyabudnyk/PycharmProjects/musicProject/features.csv'))
 
-print(split_train_test(split_variables('/Users/zhenyabudnyk/PycharmProjects/musicProject/features.csv')[0],
-                       split_variables('/Users/zhenyabudnyk/PycharmProjects/musicProject/features.csv')[1]))
+print(scaler(split_train_test(split_variables('/Users/zhenyabudnyk/PycharmProjects/musicProject/features.csv')[0],
+                       split_variables('/Users/zhenyabudnyk/PycharmProjects/musicProject/features.csv')[1])[0], split_train_test(split_variables('/Users/zhenyabudnyk/PycharmProjects/musicProject/features.csv')[0],
+                       split_variables('/Users/zhenyabudnyk/PycharmProjects/musicProject/features.csv')[1])[1]))
