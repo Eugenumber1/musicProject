2f2683ebdaba7274ec8e9cd7f4ddc39bfc283e02
s2422980 <y.budnyk@student.utwente.nl>
2022-07-19 16:14:09 +0200

    ANN is done, by changing the input shape parameter, the accuracy now is 0.98 percent, which can still be optimized

--- machine_learning_model/ann_deep_learning.py
+++ machine_learning_model/ann_deep_learning.py
@@ -9,13 +9,13 @@ import numpy as np
 # this method will build an artificial neural network
 def ann_make(X_train, y_train):
     ann = keras.models.Sequential()
-    ann.add(keras.layers.Dense(units=16, input_shape=(None, 1), activation='relu'))
-    ann.add(keras.layers.Dense(units=32, activation='relu'))
+    ann.add(keras.layers.Dense(units=128, input_shape=(None, 11), activation='relu'))
+    ann.add(keras.layers.Dense(units=512, activation='relu'))
     ann.add(keras.layers.Dense(units=10, activation='softmax'))
     ann.add(keras.layers.Flatten())
     ann.summary()
     ann.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    ann.fit(X_train, y_train, batch_size=16, epochs=100)
+    ann.fit(X_train, y_train, batch_size=32, epochs=100)
     return ann
 
 # this method turns the y labels into numerical values from categorical
@@ -31,8 +31,8 @@ def run(path):
     #df = pd.read_csv(path)
     #df.info()
     X_train, X_test, y_train, y_test = data_preprocessing.preprocess(path, standard_scaler=True)
-    print(X_train)
-    print(y_train)
+    #print(X_train)
+    #print(y_train)
     #X_train = X_train.reshape(1, -1)
     #X_test = X_test.reshape(1, -1)
     y_train_enc, y_test_enc = prepare_targets(y_train, y_test)
@@ -43,11 +43,11 @@ def run(path):
     print(X_test.shape)
     print(y_train.shape)
     print(y_test.shape)
-    print(X_train)
-    print(y_train)
+    #print(X_train)
+    #print(y_train)
     ann = ann_make(X_train, y_train)
     predictions = ann.predict(X_test)
-    print(predictions)
+    #print(predictions)
     #print(accuracy_estimator(y_test, predictions))
     #ann.evaluate()
 
